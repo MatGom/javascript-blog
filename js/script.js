28589396f9fb1bb0959f2087fad4ffcde8c2e8b2
Mateusz Gomolka <mdm.gomolka@gmail.com>
2022-08-11 21:53:35 +0100

    Fix for of links bug

--- js/script.js
+++ js/script.js
@@ -37,12 +37,6 @@
 		targetArticle.classList.add('active');
 	};
 
-	const links = document.querySelectorAll('.titles a');
-
-	for (let link of links) {
-		link.addEventListener('click', titleClickHandler);
-	}
-
 	const optArticleSelector = '.post',
 		optTitleSelector = '.post-title',
 		optTitleListSelector = '.titles';
@@ -58,29 +52,33 @@
 		const articles = document.querySelectorAll(optArticleSelector);
 
 		let html = '';
-		
+
 		for (let article of articles) {
-			/* get the article id */
+			/* [DONE] get the article id */
 
 			const articleId = article.getAttribute('id');
 
-			/* find the title element */
-			/* get the title from the title element */
+			/* [DONE] find the title element */
+			/* [DONE] get the title from the title element */
 
 			const articleTitle = article.querySelector(optTitleSelector).innerHTML;
 
-			/* create HTML of the link */
+			/* [DONE] create HTML of the link */
 
 			const linkHTML = '<li><a href="#' + articleId + '"><span>' + articleTitle + '</span></a></li>';
 
-			/* insert link into titleList */
+			/* [DONE] insert link into titleList */
 
 			html = html + linkHTML;
-
-			console.log(html);
 		}
 
 		titleList.innerHTML = html;
+
+		const links = document.querySelectorAll('.titles a');
+
+		for (let link of links) {
+			link.addEventListener('click', titleClickHandler);
+		}
 	};
 
 	generateTitleLinks();
